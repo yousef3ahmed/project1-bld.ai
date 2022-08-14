d6c89786d07b268672944595c58c9984f989c368
yousef ahmed <yousef.amer5453@gmail.com>
2022-08-14 05:18:00 +0200

    finall

--- templetes/post.js
+++ templetes/post.js
@@ -1,10 +1,19 @@
  
  
 
-  function listsPosts( id ){
-    
+function ok( big , small ){
+    return big.includes( small ) ;
+}  
+
+
+function listsPosts( id , value ){
+
     const box = document.getElementById(id);
-    
+  
+    while (box.firstChild) {
+        box.removeChild(box.lastChild);
+      }
+
     fetch('../data-app-server/data.json')
     .then(response => response.json())
     .then(json => {
@@ -12,28 +21,39 @@
         
         for (const key in json) {
             for (const key2 in json[key]) {
-                
-                box.appendChild( postCourse( json[key][key2] ) );
+             
+                let big = json[key][key2].description ;
+               
+                if( ok( big , value ) == true ){
+                    console.log( "fuck" );
+                    box.appendChild( postCourse( json[key][key2] ) );
+                }
+                     
             }  
         }    
     });
-    
- }
 
- function postCourse( post ){
-    
+}
+
+function postCourse( post ){
+
     const Course = document.createElement('div');
     Course.setAttribute('class' , 'courses');
-    Course.textContent = 'Hello world';
-    
-    console.log( "yousef" );
 
+    const IMG = document.createElement('img');
+    IMG.src = post.image ;
+    IMG.setAttribute( "class" , "coursesImg" );
+    Course.appendChild( IMG );
+
+    const H1 = document.createElement('h2');
+    H1.textContent = post.description;
+    Course.appendChild( H1 );
 
     const PP = document.createElement( 'p' ) ;
     PP.textContent = post.title ;
-    PP.setAttribute("class" , "courses") ;
+    PP.setAttribute("class" , "coursesP") ;
     Course.appendChild( PP ) ;
 
     return Course;
- }
+}
  
