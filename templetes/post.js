f9c1e8f76c2ece74690561fba001be469010ab0d
yousef ahmed <yousef.amer5453@gmail.com>
2022-08-17 01:22:49 +0200

    phase3

--- templetes/post.js
+++ templetes/post.js
@@ -5,8 +5,9 @@ function ok( big , small ){
     return big.includes( small ) ;
 }  
 
+let curCorce = "" ;
 
-function listsPosts( id , value ){
+function listsPosts( id , value , compare ){
 
     const box = document.getElementById(id);
   
@@ -17,19 +18,21 @@ function listsPosts( id , value ){
     fetch('../data-app-server/data.json')
     .then(response => response.json())
     .then(json => {
-        console.log(json);
         
         for (const key in json) {
-            for (const key2 in json[key]) {
-             
-                let big = json[key][key2].description ;
-               
-                if( ok( big , value ) == true ){
-                    console.log( "fuck" );
-                    box.appendChild( postCourse( json[key][key2] ) );
+            
+
+            if( key.includes( compare )  ){
+                curCorce = compare ;
+                for (const key2 in json[key]) {
+                    let big = json[key][key2].description ;
+                    
+                    if( ok( big , value ) == true ){
+                        box.appendChild( postCourse( json[key][key2] ) );
+                    }
+                         
                 }
-                     
-            }  
+            }   
         }    
     });
 
@@ -38,22 +41,68 @@ function listsPosts( id , value ){
 function postCourse( post ){
 
     const Course = document.createElement('div');
-    Course.setAttribute('class' , 'courses');
+    Course.setAttribute('class' , 'card');
+    Course.style = "width: 18rem;"
 
     const IMG = document.createElement('img');
     IMG.src = post.image ;
-    IMG.setAttribute( "class" , "coursesImg" );
+    IMG.setAttribute( "class" , "card-img-top" );
     Course.appendChild( IMG );
 
-    const H1 = document.createElement('h2');
-    H1.textContent = post.description;
-    Course.appendChild( H1 );
 
-    const PP = document.createElement( 'p' ) ;
-    PP.textContent = post.title ;
-    PP.setAttribute("class" , "coursesP") ;
-    Course.appendChild( PP ) ;
+    const innerDiv = document.createElement('div');
+    innerDiv.setAttribute( 'class' , 'card-body' );
+
+
+
+    const PP = document.createElement('p');
+    PP.setAttribute( 'class' , 'card-text' );
+    PP.textContent = post.title;
+    innerDiv.appendChild( PP );
+
+    const H5 = document.createElement( 'h5' ) ;
+    H5.textContent = post.description ;
+    H5.setAttribute("class" , "card-title") ;
+    innerDiv.appendChild( H5 ) ;
+
+    const AA = document.createElement('a');
+    AA.href = '#' ;
+    AA.setAttribute( 'class' , 'btn btn-primary' ) ;
+    AA.innerHTML = "Go somewhere" ;
+    innerDiv.appendChild( AA ) ;
+
+
+    Course.appendChild( innerDiv );
+    
 
     return Course;
 }
+
+
+//////////////////////////////////////////  froms  ///////////////////////////////////////////////////////////////////
+
+
+function doForm(){  
+    let value = "" ; 
+   var data = new FormData();
+   data.append( "" , document.getElementById("search").value );
+   for (let x of data.entries()) { 
+       
+       let out = x.toString() ; 
+       for( let i = 1 ; i < out.length ; i++ ) value += out.charAt( i ) ;
+       console.log(value); 
+   }
+   listsPosts( 'box' , value , curCorce );
+   value = "";
+   return false ; 
+}
+
+
+
+
+
+function Choose( string ){
+    let x =  document.getElementById(string).value;
+    listsPosts( 'box' , "" , string );
+ }
  
